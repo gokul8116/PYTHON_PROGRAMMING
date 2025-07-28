72a1ef3f23ca0213f56073c2d344eda578e2857c
GOKUL PAL <137334469+gokul8116@users.noreply.github.com>
2025-07-28 21:12:26 +0530

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,18 @@
+a="gokul"
+b=365
+c=45.87
+d =987876.9
+print("a")
+print("b")
+print("c")
+# aise variable ka andor ka value store nehi hoga
+print(a)
+print(b)
+print(c)
+print(d)
+#aise store hoga without inverted koma
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+#aise variable ka type bata denga
